0dbcd3b28ff81c765618e218c313b4814675522b
Palace0-0 <g.palace@hotmail.com>
2025-11-03 18:50:21 -0300

    Popular tabuleiro
    Agora o tabuleiro é prenchido corretamente, foi utilizado um método com backtrakcing, se nãao as vezes algumas células eram prenchidas com 0.

--- Sudoku/src/sudoku/Sudoku.java
+++ Sudoku/src/sudoku/Sudoku.java
@@ -2,6 +2,7 @@
 package sudoku;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -9,12 +10,12 @@ public class Sudoku {
     
     private int [][] tabuleiro = new int [9][9];
     
-    private boolean verificarposicao(int valor, int[][]tabuleiro, int linha, int coluna){
+    private boolean verificarposicao(int valor, int linha, int coluna){
         
         //Verifica se o valar a ser add ja existe na linha
         for (int i = 0; i < tabuleiro.length; i++) {
             if (valor == tabuleiro[linha][i]){
-                System.out.println(valor + "já existe na linha");
+                //System.out.println(valor + "já existe na linha");
                 return false;
             }
         }
@@ -22,7 +23,7 @@ public class Sudoku {
         //Verifica se o valar a ser add ja existe na coluna
         for (int j = 0; j < tabuleiro.length; j++) {
             if (valor == tabuleiro[j][coluna]){
-                System.out.println(valor + "já existe na coluna");
+                //System.out.println(valor + "já existe na coluna");
                 return false;
             }
         }
@@ -34,7 +35,7 @@ public class Sudoku {
         for (int i = inicioLinha; i < inicioLinha + 3; i++) {
             for (int j = inicioColuna; j < inicioColuna + 3; j++) {
                 if (tabuleiro[i][j] == valor) {
-                    System.out.println(valor + "já existe na matriz 3x3");
+                    //System.out.println(valor + "já existe na matriz 3x3");
                     return false;
                 }
             }
@@ -43,17 +44,47 @@ public class Sudoku {
         return true;
     }
     
-    public void populartabuleiro(){
-        // Cria a lista uma vez
-        List<Integer> numeros = new ArrayList<>();
-        for (int n = 1; n <= 9; n++) numeros.add(n);
-        
-        //Valida se o valor pode ser inserido e o inseri
+     // Preenche o tabuleiro usando backtracking
+    public boolean populartabuleiro() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (tabuleiro[i][j] == 0) { // se a célula estiver vazia
+
+                    // Cria lista de números 1-9 e embaralha para aleatoriedade
+                    List<Integer> numeros = new ArrayList<>();
+                    for (int n = 1; n <= 9; n++) numeros.add(n);
+                    Collections.shuffle(numeros);
+
+                    for (int valor : numeros) {
+                        if (verificarposicao(valor, i, j)) {
+                            tabuleiro[i][j] = valor;
+
+                            // Chamada recursiva
+                            if (populartabuleiro()) {
+                                return true; // encontrou solução
+                            }
+
+                            // Se não der certo, desfaz (backtrack)
+                            tabuleiro[i][j] = 0;
+                        }
+                    }
+
+                    // Nenhum número funcionou nesta célula, volta
+                    return false;
+                }
+            }
+        }
+
+        // Todas as células foram preenchidas
+        return true;
+    }
+    
+    public void printartabuleiro (){
         for (int i = 0; i < tabuleiro.length; i++) {
             for (int j = 0; j < tabuleiro.length; j++) {
-                
+                System.out.print(tabuleiro[i][j]);
             }
-            
+            System.out.println("");
         }
     }
     
@@ -62,5 +93,8 @@ public class Sudoku {
     public static void main(String[] args) {
        Sudoku jogo = new Sudoku();
        jogo.populartabuleiro();
+       jogo.printartabuleiro();
+       
+      
     }
 }
